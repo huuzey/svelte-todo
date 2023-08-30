9a9c8afe9300fe269166f82aa389681d53124ec1
huuzey <weleladinsefa@gmail.com>
2023-08-30 16:48:18 +0300

    at one

---
+++ src/lib/firebase/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { deleteApp, getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "svelte-todo-f5584.firebaseapp.com",
+  projectId: "svelte-todo-f5584",
+  storageBucket: "svelte-todo-f5584.appspot.com",
+  messagingSenderId: "358514863823",
+  appId: "1:358514863823:web:4a39e360dc291c7124d1ba",
+};
+
+// Initialize Firebase
+let firebaseApp;
+if (!getApps().length) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = getApp();
+  deleteApp(firebaseApp);
+  firebaseApp = initializeApp(firebaseConfig);
+}
+
+export const db = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
